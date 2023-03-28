1546118896eff39f447ca08f73be104d219c087e
thimethane <thimethane@gmail.com>
2023-03-28 06:59:16 -0700

    Up to date

--- output.c
+++ output.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
 
 /**
  * _putchar - Writes a character to the standard output
